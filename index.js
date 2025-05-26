8283c863105ec2763173cfad528f14f8171ec2ca
zhovatter <zh6603@gmail.com>
2025-05-26 10:23:49 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+//next steps, have better interface for showing current operations, control structure allowing multi-digit numbers, implement reset, handle invalid operation sequences, style
+//future implementation: decimal support, and use a data structure to allow an arbitrary amount of numbers and operators in a single line, rather than just 2 numbers and 1 operator
+
+function increment(elementID){
+    //var output = document.getElementById('result');
+    result++;
+    output.innerHTML = result; 
+    console.log(result);
+}
+
+function reset() {
+    result = 0;
+    num1 = 0;
+    num2 = 0;
+    currOp = null;
+    calculation.innerHTML = "";
+    output.innerHTML = 0;
+}
+
+function selectNum(num) {
+    if (currOp == null) {
+        if (result !== 0) {
+            console.log("attempting erase of calc");
+            num1 = 0;
+            calculation.innerHTML = "";
+        }
+        num1 = num1*10 + num //allows multi-digit inputs
+    } else {
+        num2 = num2*10 + num;
+    }
+    calculation.innerHTML += num;
+}
+
+function selectOp(operator) {
+    num2 = 0; //resetting num2 for new op
+    currOp = operator;
+    calculation.innerHTML = num1 + operator;
+}
+
+function eval() {
+    if (currOp == "+") {
+        result = num1+num2;
+    } else if (currOp == "-") {
+        result = num1-num2;
+    } else if (currOp == "*") {
+        result = num1*num2;
+    } else if (currOp == "/") {
+        result = num1/num2;
+    }
+    num1 = result
+    //var output = document.getElementById('result');
+    output.innerHTML = result;
+    calculation.innerHTML = num1
+    currOp = null;
+
+}
+
+var result = 0;
+let num1 = 0;
+let num2 = 0;
+var currOp;
+let calculation = document.getElementById('calculation')
+let output = document.getElementById('result');
+
